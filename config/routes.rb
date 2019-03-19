de56285c90309e61752737402435f26350c6627c
Mohammed-Elsamman <monem7242@gmail.com>
2019-03-19 23:53:26 +0200

    create view and controller for sign_in and sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup',to:"signup#signup"
+  get 'signin',to:"signin#signin"
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
