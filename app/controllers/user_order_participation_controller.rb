a011f00cdb70668bdc1d27e6daad3412230b22b5
Mohammed-Elsamman <monem7242@gmail.com>
2019-03-27 21:55:02 +0200

    more validation on order model and edit in order details view and controller

--- app/controllers/user_order_participation_controller.rb
+++ app/controllers/user_order_participation_controller.rb
@@ -6,12 +6,15 @@ class UserOrderParticipationController < ApplicationController
     # debugger
     @order=Order.find(params[:order_id])
     @addOrder = UserOrderParticipation.new
-    @orderInvited = UserOrderInvitation
-    .where('user_id <> ?', current_user.id).group(:user_id).where(order_id: params[:order_id])
+    @orderInvited = UserOrderInvitation.where(order_id: params[:order_id])
+                      # .where('user_id <> ?',UserOrderParticipation.where(order_id: params[:order_id],))
+    # .where('user_id <> ?', current_user.id)
+    # .group(:user_id)
+
     @orderPartic = UserOrderParticipation
                        .where(order_id: params[:order_id])
                        .group(:user_id)
-                       .where('user_id <> ?', current_user.id)
+                       # .where('user_id <> ?', current_user.id)
     @orderImg = Order.find(params[:order_id])
 
   end
@@ -29,17 +32,27 @@ class UserOrderParticipationController < ApplicationController
   end
 
   def create
-  @addOrder = UserOrderParticipation.new
-  @addOrder.user_id = current_user.id
-  @addOrder.item = params[:item]
-  @addOrder.order_id = params[:order_id]
-  @addOrder.price = params[:price]
-  @addOrder.amount = params[:amount]
-  @addOrder.comment = params[:comment]
-    if @addOrder.save
-      redirect_to "/orders/" + params[:order_id] + "/orderdata"
+    @orderStatus = Order.find(params[:order_id])
+    # $ErrorOrder=[]
+    if @orderStatus.status == 'waiting'
+        @addOrder = UserOrderParticipation.new
+        @addOrder.user_id = current_user.id
+        @addOrder.item = params[:item]
+        @addOrder.order_id = params[:order_id]
+        @addOrder.price = params[:price]
+        @addOrder.amount = params[:amount]
+        @addOrder.comment = params[:comment]
+
+        if !@addOrder.save
+          $ErrorOrder =@addOrder.errors.full_messages
+        end
+    else
+      $ErrorOrder ="this order is done 1000 hana w shfa";
     end
+    redirect_to order_user_order_participation_index_path;
+
   end
+
 
   private
   def addOrder_params
--- app/models/user_order_participation.rb
+++ app/models/user_order_participation.rb
@@ -1,4 +1,11 @@
 class UserOrderParticipation < ApplicationRecord
   belongs_to :user
   belongs_to :order
+
+  validates :item, :price, :amount, presence: true
+  validates :item, format: { with: /\A[a-zA-Z0-9]+\z/,
+                                    message: "pleas : no special characters " }
+  validates_inclusion_of :price, :in => 3..99
+  validates_inclusion_of :amount, :in => 1..100
+  validates :amount, numericality: { only_integer: true }
 end
