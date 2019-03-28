4fab20a73ed14336660f6c96e53e353a647eeec3
moatasemebrahim <motasemebrahim.m@gmail.com>
2019-03-28 15:54:12 +0200

    sorting notification by time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,50 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
-    before_action :current_user
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
 
         
-    @allInfo = [];
-    @keys = [];
-    @allNotifications = [];
-    @counter=0
-
-    @myInvitaions = UserOrderInvitation.where(user_id: @currentUser)
-      @myInvitaions.each do |invitation|
-      @key = invitation.order.created_at.to_i;
-      @allInfo.push({
-      "type" => "inv",
-      "name" => invitation.order.user.name,
-      "order_id" => invitation.order.id ,
-      "category" => invitation.order.category,
-      "restaurant" => invitation.order.resturant,
-      "key" => @key})
-      @keys.push(@key);
-    end
-
-
-    current_user.orders.each do |order|
-      order.UserOrderParticipations.each do |participation|
-      @key = order.created_at.to_i;
-      @allInfo.push({
-      "type" => "join",
-      "name" => participation.user.name,
-      "order_id" => participation.order.id ,
-      "key" => @key})
-      @keys.push(@key);
-      end
-    end
-
-    @newkeys = @keys.sort.reverse
-    @newkeys.each do |key|
-      @allInfo.each do |info|
-        if key == info['key']
-          @allNotifications.push(info);
-        end
-      end
-    end
 
 
     protected
